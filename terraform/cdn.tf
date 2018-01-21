2d0a736e3e69d99ac9815ee97ebde67e2cc1797f
Nathan Toups <n@rjrbt.io>
2018-01-21 16:12:48 -0600

    updated terraform to resolve index.html more predictably

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -1,8 +1,20 @@
 resource "aws_cloudfront_distribution" "site" {
   origin {
-    domain_name = "${var.site["bucket"]}.s3.amazonaws.com"
+    domain_name = "${var.site["bucket"]}.s3-website-us-east-1.amazonaws.com"
     origin_id   = "S3-${var.site["bucket"]}"
+    custom_origin_config {
+      http_port = "80"
+      https_port = "443"
+      origin_protocol_policy = "http-only"
+      origin_keepalive_timeout = "5"
+      origin_read_timeout = "30"
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+
   }
+
+
+
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "${var.site["name"]}"
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -4,6 +4,13 @@ resource "aws_s3_bucket" "site" {
     versioning {
         enabled = true
     }
+
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+
+    }
+
     tags {
         Name =  "${var.site["name"]}"
         Environment = "${var.site["environment"]}"

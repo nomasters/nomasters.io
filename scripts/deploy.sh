cdf74e0c174b35611b12d2f5fc59dfe45c3d0e02
Nathan Toups <n@rjrbt.io>
2020-03-11 07:07:31 -0600

    trying new orbs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,9 @@ set -e
 DIR=site
 BUCKET=s3://nomasters.io
 DIST_ID=$NOMASTERS_CDN_DIST_ID
-CACHE_ID=`date +%s`
+CACHE_ID=$(date +%s)
 FORMATTED_JSON='{"Paths": {"Quantity": 1,"Items": ["/*"]},"CallerReference":"'${CACHE_ID}'"}'
 
 cd $DIR
-hugo
 aws s3 sync public/ $BUCKET --acl public-read
-aws cloudfront create-invalidation --distribution-id $DIST_ID --invalidation-batch "$FORMATTED_JSON"
+aws cloudfront create-invalidation --distribution-id "$DIST_ID" --invalidation-batch "$FORMATTED_JSON"
